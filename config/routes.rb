96ed8169b4df6888062920472c8a275e7e0b8516
bojosteph <bojosteph@gmail.com>
2019-04-29 15:49:28 -0700

    Added public controller and home page in views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-  get 'users/new'
-  get 'users/edit'
+  #get 'users/new'
+  #get 'users/edit'
   #get 'events/new'
   #get 'events/edit'
   #get 'events/index'
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :events
   resources :users
+  root :to => 'home#index'
 end
