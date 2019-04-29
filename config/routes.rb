a16866281933eda4fd18a73cf775dcda9e47ea35
bojosteph <bojosteph@gmail.com>
2019-04-29 12:32:12 -0700

    added User model and events controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  #get 'events/new'
+  #get 'events/edit'
+  #get 'events/index'
+  #get 'events/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :events
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_29_172028) do
+ActiveRecord::Schema.define(version: 2019_04_29_191119) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2019_04_29_172028) do
     t.integer "planner_id"
     t.datetime "start_date"
     t.datetime "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
