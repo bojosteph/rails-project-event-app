59db4de0e07d82a977bbb5610508957f8ffa79e2
bojosteph <bojosteph@gmail.com>
2019-04-29 13:41:00 -0700

    updated events controller to add create and edit , added views and forms for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,54 @@
 class EventsController < ApplicationController
-  def new
-  end
-
-  def edit
-  end
-
   def index
-  end
+        @events = Event.all
+    end
 
-  def show
-  end
+       
+    def show 
+        @event = Event.find(params[:id])
+    end
+
+    def new 
+        @event = Event.new 
+    end
+
+    def create 
+        #@user = current_user
+        @event = Event.new(event_params(:name, :location, :description, :planner_id, :start_date, :end_date))
+
+        if @event.save
+            redirect_to event_path(@event)
+        else 
+            render :new 
+        end
+    end
+
+    def edit 
+        @event = Event.find(params[:id])
+    end
+
+    def update 
+        @event = Event.find(params[:id])
+        if @event.update(event_params(:name, :date, :location, :description, :planner_id, :start_date, :end_date))
+            redirect_to event_path(@event)
+        else
+            render :edit 
+        end
+    end
+
+    def destroy
+        Event.find(params[:id]).delete
+        redirect_to events_path
+    end
+
+    private 
+
+    def event_params(*args)
+        params.require(:event).permit(*args)
+    end
+
+    
+
 end
+
+
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,3 @@
 class Event < ApplicationRecord
+    belongs_to :planner, :class_name => "User", :foreign_key => 'planner_id', optional: true
 end
