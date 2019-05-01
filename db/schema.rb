5c627c2ad2554d95aa7f4032ebcf6edeaf6dcd7f
github username <github email address>
2019-05-01 14:06:03 -0400

    added ggogle token with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_003309) do
+ActiveRecord::Schema.define(version: 2019_05_01_180326) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2019_05_01_003309) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "google_token"
+    t.string "google_refresh_token"
   end
 
 end
